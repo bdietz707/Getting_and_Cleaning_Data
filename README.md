6ef428fcf2ca6418725ef3f6719b87d3513a2257
bdietz <bdietz@alum.mit.edu>
2015-09-25 22:02:20 -0500

    Initial Upload

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# Getting_and_Cleaning_Data
+#Getting and Cleaning Data Course Project
+##README.md
+Brad Dietz
+
+###Requirements: 
+
+1. Merges the training and the test sets to create one data set.
+2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+3. Uses descriptive activity names to name the activities in the data set.
+4. Appropriately labels the data set with descriptive variable names. 
+5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+###Setup:
+
+1. Download the data source and put into a folder on your local drive. You'll have a UCI HAR Dataset folder.
+2. Put run_analysis.R in the parent folder of UCI HAR Dataset, then set it as your working directory using setwd() function in RStudio.
+3. Run source("run_analysis.R"), then it will generate a new file tiny_data.txt in your working directory.
+
+###Using run_analysis.R:
+
+1. Load run_analysis.R into R Studio
+2. Set your working directory using setwd() function in Rstudio
+3. Run the full script
+
+###Dependencies:
+
+1. Automatically downloads and unzips the dataset 
+2. Automatically loads R library dependencies (plyr)
+
+###run_analysis.R process:
+
+1. Downloads and unzips the dataset if it does not already exist in the working directory
+2. Load the column labels (activity and feature info)
+3. Loads the training sets, changes the column names and binds the subject and activity columns to the base training set
+4. Removes unnecessary training tables
+5. Loads the testing sets, changes the column names and binds the subject and activity columns to the base testing set
+6. Removes unnecessary testing tables
+7. Creates a full dataset by binding the rows of the testing set to the training set
+8. Removes training and testing sets
+9. Keep only the std, mean, subject, and activity columns
+10. Label the activity column with the descriptive variable names
+11. Create an independent tidy data set with the average of each variable for each activity and each subject
+12. Create a text file of the averages data set
+
+The end result is shown in the file column_averages.txt.
+
---
+++ run_analysis.R
@@ -0,0 +1,78 @@
+filename <- "getdata_dataset.zip" 
+
+#Download the dataset if it does not exist
+if (!file.exists(filename)){ 
+   fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip" 
+   download.file(fileURL, filename) 
+}   
+
+#Unzip the dataset if it does not exist
+if (!file.exists("UCI HAR Dataset")) {  
+   unzip(filename)  
+} 
+
+#Load Labels
+features <- read.table("UCI HAR Dataset/features.txt")
+column_names <- features[,2]
+activity <- read.table("UCI HAR Dataset/activity_labels.txt")
+
+#Load the training, subject, and activity sets
+xtrain <- read.table("UCI HAR Dataset/train/x_train.txt")
+ytrain <- read.table("UCI HAR Dataset/train/y_train.txt")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt")
+
+#Rename the training columns
+colnames(xtrain) <- column_names
+colnames(subject_train) <- "subject"
+colnames(ytrain) <- "activity"
+
+#Bind the columns of the subject and activity data to the training data set 
+xtrain <- cbind(xtrain, subject_train)
+xtrain <- cbind(xtrain, ytrain)
+
+#Remove unecessary data
+rm(subject_train)
+rm(ytrain)
+rm(features)
+
+#Load the testing, subject and activity sets
+xtest <- read.table("UCI HAR Dataset/test/X_test.txt")
+ytest <- read.table("UCI HAR Dataset/test/y_test.txt")
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt")
+
+#Rename the testing columns
+colnames(xtest) <- column_names
+colnames(subject_test) <- "subject"
+colnames(ytest) <- "activity"
+
+#Bind the columns of the subject and activity data to the testing data set 
+xtest <- cbind(xtest, subject_test)
+xtest <- cbind(xtest, ytest)
+
+#Remove unecessary data
+rm(subject_test)
+rm(ytest)
+rm(column_names)
+
+#Bind the rows of the training and testing data sets
+full <- rbind(xtrain, xtest)
+
+#Remove unecessary data
+rm(xtest)
+rm(xtrain)
+
+#Keep only the std, mean, subject, and activity columns
+full <- full[, grepl("mean\\(\\)|std\\(\\)|subject|activity", names(full))]
+
+#Label the activity column with the descriptive variable names
+full <- merge(x=full, y=activity, by.x = c("activity"), by.y = c("V1"), all.x=TRUE)
+full <- full[,2:69]
+
+colnames(full)[68] <- "activity"
+
+#Creates an independent tidy data set with the average of each variable for each activity and each subject
+library(plyr)
+column_averages <- ddply(full, .(subject, activity), function(x) colMeans(x[,1:66]))
+
+#Create a text file of the averages data set
+write.table(column_averages, "column_averages.txt", row.name=FALSE)
